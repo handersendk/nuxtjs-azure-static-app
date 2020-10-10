8e811ae6b2607eb13edc1031502cc5f46d3ffbf9
Henrik Andersen <hea_dk@hotmail.com>
2020-10-10 23:07:47 +0200

    Added auth-next auth

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,46 @@
 export default {
-    generate:{
-        exclude: [/spa-fallback/]
-    }
-}
+  generate: {
+    exclude: [/spa-fallback/],
+  },
+  modules: ["@nuxtjs/axios", "@nuxtjs/auth-next"],
+  router: {
+    middleware: ["auth"],
+  },
+  auth: {
+    redirect: {
+      login: "/",
+      callback: "/auth",
+    },
+    strategies: {
+      aad: {
+        scheme: "oauth2",
+        endpoints: {
+          authorization:
+            "https://haptio.b2clogin.com/haptio.onmicrosoft.com/B2C_1_signupsignin1/oauth2/v2.0/authorize",
+          token:
+            "https://haptio.b2clogin.com/haptio.onmicrosoft.com/B2C_1_signupsignin1/oauth2/v2.0/token",
+          access_token_endpoint:
+            "https://haptio.b2clogin.com/haptio.onmicrosoft.com/B2C_1_signupsignin1/oauth2/v2.0/token",
+          logout: "/",
+        },
+        token: {
+          property: "access_token",
+          type: "Bearer",
+          maxAge: 1800,
+        },
+        refreshToken: {
+          property: "refresh_token",
+          maxAge: 60 * 60 * 24 * 30,
+        },
+        responseType: "code",
+        grantType: "authorization_code",
+        accessType: "offline",
+        // ******** change this for your Application (Client) ID ********
+        clientId: "293fede3-b400-4969-98bf-17f7f5837b5a",
+        codeChallengeMethod: "S256",
+        scope: ["openid", "profile"],
+        autoLogout: true,
+      },
+    },
+  },
+};
--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,16 @@
 <template>
-    <div>
-        <h1 class="title">About Page</h1>
-        <p class="subtitle">This is statically generated page</p>
-        <NuxtLink to="/">Back to Home</NuxtLink>
-    </div>
+  <div>
+    <h1 class="title">About Page</h1>
+    <p class="subtitle">This is statically generated page</p>
+    <a v-if="!this.$auth.loggedIn" @click="$auth.loginWith('aad')"
+      >Sign in with Microsoft</a
+    >
+    <a v-else @click="$auth.logout('aad')">Sign out</a>
+    <NuxtLink to="/">Back to Home</NuxtLink>
+  </div>
 </template>
+<script>
+export default {
+  auth: false,
+};
+</script>
